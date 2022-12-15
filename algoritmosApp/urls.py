11558d28e650f9ab750b9d292d887f235143b0ee
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-12-15 15:15:10 -0500

    eliminar urls que no se usan

--- algoritmosApp/urls.py
+++ algoritmosApp/urls.py
@@ -3,16 +3,13 @@ from django.urls import re_path as url
 from algoritmosApp import views
 
 urlpatterns = [
-    url(r'^graph$', views.graphApi),
-    url(r'^graph/([0-9]+)$', views.graphApi),
-    url(r'^archivo$', views.simple_upload),
-    url(r'^image$', views.img_upload),
-    url(r'^imagexc$', views.img_ex),
-    url(r'^randomgraph$', views.random_graph),
-    url(r'^xml/([0-9]+)$', views.export_xml),
-    url(r'^matriz/([0-9]+)$', views.export_matriz),
-    url(r'^quey/([0-9]+)$', views.queyrannne),
-    url(r'^cluster/([0-9]+)$', views.clustering),
+    # url(r'^graph$', views.graphApi),
+    # url(r'^graph/([0-9]+)$', views.graphApi),
+    # url(r'^archivo$', views.simple_upload),
+    # url(r'^image$', views.img_upload),
+    # url(r'^imagexc$', views.img_ex),
+    # url(r'^xml/([0-9]+)$', views.export_xml),
+    # url(r'^matriz/([0-9]+)$', views.export_matriz),
     url(r'^puzzle-estrella$', views.algoritmo_estrella),
     url(r'^puzzle-anchura$', views.algoritmo_anchura),
     url(r'^puzzle-profundidad$', views.algoritmo_profundidad),
